35c22c3aeaba944afbafdec52f23b74cbdb57a5b
Vortex-21 <anshumanlaskar1@gmail.com>
2025-01-25 12:28:53 +0530

    DB added!

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,10 +1,10 @@
 import express from "express";
 import { z } from "zod";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 import { auth } from "./middlewares";
-// import { JWT_SECRET } from "./config";
-import {SignUpSchema } from "@repo/common/types"
+import { SignUpSchema } from "@repo/common/types";
 import { JWT_SECRET } from "@repo/backend-common/config";
+import { prisma } from "@repo/Database/prismaClient";
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
@@ -26,49 +26,57 @@ app.use(express.json());
 //     ),
 // });
 
-app.post("/signup", (req, res) => {
+app.post("/signup", async (req, res) => {
   // res.send("signup route!")
-  const { username, password } = req.body;
-  const result = SignUpSchema.safeParse({username, password}); 
+  const { username, password, name = "John Doe", photo = "dp.jpg" } = req.body;
+  const result = SignUpSchema.safeParse({ username, password, name });
 
-
-  if(result.success){ 
-    // store in DB 
+  if (result.success) {
+    // store in DB
+    try{await prisma.user.create({
+      data: {
+        username,
+        password,
+        name,
+        photo
+      },
+    });
     res.status(200).json({
-        message:"Signed Up successfully!"
-    })
-  }
-  else{
+      message: "Signed Up successfully!",
+    });}
+    catch(err:any){
+      console.log("ERROR signing up: ", err);
+    }
+  } else {
     res.status(400).json({
-        message:result.error?.issues
-    })}
-
+      message: result.error?.issues,
+    });
+  }
 });
 
 // const JWT_SECRET = "my_jwt_secret"
 
-app.post("/signin", (req,res) => {
-    const {username, password} = req.body; 
+app.post("/signin", (req, res) => {
+  const { username, password } = req.body;
 
-    //check the DB for the creds 
+  //check the DB for the creds
 
+  //if not present in the DB dont log in
 
-    //if not present in the DB dont log in 
+  //if present sign jwt send back to the user :
+  const token = jwt.sign({ username: username }, JWT_SECRET);
+  res.status(200).json({
+    token: token,
+    message: "Signed in successfully!",
+  });
+});
 
-    //if present sign jwt send back to the user : 
-    const token = jwt.sign({username:username}, JWT_SECRET); 
-    res.status(200).json({
-        token:token, 
-        message:"Signed in successfully!"
-    })
-})
-
-app.post("/create-room",auth, (req,res) => {
-    console.log("id : ", req.id); 
-    res.status(200).json({
-        message:"created room!"
-    })
-})  
+app.post("/create-room", auth, (req, res) => {
+  console.log("id : ", req.id);
+  res.status(200).json({
+    message: "created room!",
+  });
+});
 
 app.get("/", (req, res) => {
   res.send("Welcome to Excalidraw");
