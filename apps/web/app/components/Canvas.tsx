a84bec868e9fb2755d10a558a70c328b4be67512
Vortex-21 <anshumanlaskar1@gmail.com>
2025-02-22 13:50:19 +0530

    Cleaned up previous code and changed main-canvas's stroke background colors

--- apps/web/app/components/Canvas.tsx
+++ apps/web/app/components/Canvas.tsx
@@ -1,133 +1,4 @@
-// "use client";
-// import React, { useContext, useEffect, useRef, useState } from "react";
-// import axios from "axios";
-// import { ToastContainer } from "react-toastify";
-// import { initDraw } from "../../draw";
-// import { notify } from "../../utils";
-// import { ErrorPage } from "./error";
-// import { checkCookies } from "../utils/cookie-check";
-// import { globalContext, GlobalContextProvider } from "../context/GlobalContext";
-// // import { useGlobalContext } from "../context/useGlobalContext";
-// enum Shapes {
-//   rectangle = "rectangle",
-//   circle = "ellipse",
-// }
-
-// export const Canvas = ({ roomId, ws }: { roomId: number; ws: WebSocket }) => {
-//   // const windowSize = useWindowSize();
-//   // console.log("windowSize : ", windowSize);
-//   const canvasRef = useRef<HTMLCanvasElement | null>(null);
-//   const overlayCanvasRef= useRef<HTMLCanvasElement | null>(null);
-//   // const [selectedShape, setSelectedShape] = useState<Shapes>(Shapes.rectangle);
-//   const context = useContext(globalContext);
-//   if(!context)return;
-//   const {tool, setTool} =context;
-//   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(true);
-//   const [canvasSize, setCanvasSize] = useState<{ width: number; height: number } | null>(null);
-
-//   useEffect(() => {
-//     if (!canvasSize) {
-//       setCanvasSize({
-//         width: window.innerWidth,
-//         height: window.innerHeight
-//       });
-//     }
-//   }, [canvasSize]);
-
-//   // useEffect(() => {
-//   //   if (canvasRef.current && overlayCanvasRef.current) {
-//   //     const canvas = canvasRef.current;
-//   //     const ctx = canvas.getContext("2d");
-//   //     const overlayCanvas = overlayCanvasRef.current;
-//   //     if (!ctx || !roomId ||!overlayCanvas) return;
-
-//   //     ctx.fillStyle = "rgba(0,0,0)";
-//   //     ctx.fillRect(0, 0, canvas.width, canvas.height);
-//   //     initDraw(roomId, canvas, overlayCanvas, selectedShape, ws);
-//   //   }
-//   // }, [selectedShape]);
-
-//   useEffect(() => {
-
-//     checkCookies(setIsAuthenticated);
-
-//     if (canvasRef.current) {
-//       const canvas = canvasRef.current;
-//       const overlayCanvas = overlayCanvasRef.current;
-//       const ctx = canvas.getContext("2d");
-//       if (!ctx || !roomId || !overlayCanvas) return;
-//       // if (!roomId) return;
-
-//       ctx.fillStyle = "rgba(0,0,0)";
-//       ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-//       initDraw(roomId, canvas, overlayCanvas, ws);
-//     }
-//     // console.log("Dimensions : ", windowSize.width, windowSize.height);
-//   }, [canvasRef]);
-
-//   async function shareHandler() {
-//     const response = await axios.post(
-//       `http://localhost:3002/api/v1/share-room/`,
-//       { roomId: Number(roomId) },
-//       { withCredentials: true }
-//     );
-//     console.log(response.data.shareId);
-//     window.navigator.clipboard.writeText(response.data.shareId);
-//     notify("ShareId copied to clipboard!", true);
-//   }
-
-//   return isAuthenticated ? (
-//     <div className="flex  justify-center items-center gap-1p-2">
-//       <ToastContainer />
-//       <nav className="rounded-lg py-2 px-4 w-[30%] bg-white z-20 absolute top-5 left-[50%] translate-x-[-50%]">
-//         <div className="flex items-center justify-evenly">
-//           <div
-//             className={`${tool === Shapes.rectangle ? "bg-white" : ""} border rounded-lg py-2 px-4`}
-//             onClick={(e) => {
-//               setTool(Shapes.rectangle);
-//             }}
-//           >
-//             rectangle
-//           </div>
-
-//           <div
-//             className={`${tool === Shapes.circle ? "bg-white" : ""} border rounded-lg py-2 px-4`}
-//             onClick={(e) => {
-//               setTool(Shapes.circle);
-//             }}
-//           >
-//             circle
-//           </div>
-//           <div onClick={shareHandler} className='border'>Share</div>
-//           {/* <button onClick={startSessionHandler}>Start Session</button> */}
-//         </div>
-//       </nav>
-//       <div className="w-full min-h-screen">
-//         <canvas
-//           className="cursor-crosshair bg-black touch-none"
-//           ref={canvasRef}
-//           width={canvasSize?.width}
-//           height={canvasSize?.height}
-//           id="drawingCanvas"
-//         ></canvas>
-//         <canvas
-//           className="cursor-crosshair absolute  top-0 left-0 z-10 touch-none"
-//           ref={overlayCanvasRef}
-//           width={canvasSize?.width}
-//           height={canvasSize?.height}
-//           id="overlayCanvas"
-
-//         ></canvas>
-//       </div>
-//     </div>
-//   ) : (
-//     <ErrorPage />
-//   );
-// };
-// export default Canvas;
-
-import React, { MouseEventHandler, useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 interface shapeMetaData {
   x: number;
   y: number;
@@ -182,7 +53,7 @@ const Canvas = ({ roomId, ws }: { roomId: number; ws: WebSocket }) => {
     overlayCtx.scale(scale, scale);
     mainCtx.scale(scale, scale);
     overlayCtx.strokeStyle = "red";
-    mainCtx.strokeStyle = 'blue';
+    mainCtx.strokeStyle = 'red';
     overlayCtx.lineWidth = 2;
     mainCtx.lineWidth = 2;
     hist.forEach((el: shapeMetaData) => {
@@ -275,7 +146,7 @@ const Canvas = ({ roomId, ws }: { roomId: number; ws: WebSocket }) => {
       console.log("drawing done!")
       setIsDrawing(false);
       overlayCtx.clearRect(0, 0, overlayCanvas.width, overlayCanvas.height);
-      mainCtx.strokeStyle = "blue";
+      mainCtx.strokeStyle = "red";
       mainCtx.lineWidth = 2;
       mainCtx.save();
       // mainCtx.clearRect(0, 0, mainCanvas.width, mainCanvas.height); //clear the main canvas.
@@ -316,7 +187,7 @@ const Canvas = ({ roomId, ws }: { roomId: number; ws: WebSocket }) => {
       mainCtx.clearRect(0, 0, mainCanvas.width, mainCanvas.height);
       mainCtx.translate(panOffset.x*scale - scaleOffset.x, panOffset.y*scale - scaleOffset.y);
       mainCtx.scale(scale, scale); 
-      mainCtx.strokeStyle = "blue";
+      mainCtx.strokeStyle = "red";
       mainCtx.lineWidth = 2;
       hist.forEach((el) => {
         mainCtx.beginPath();
@@ -386,7 +257,7 @@ const Canvas = ({ roomId, ws }: { roomId: number; ws: WebSocket }) => {
       <canvas
         id="main"
         ref={mainCanvasRef}
-        className="bg-white touch-none"
+        className="bg-[#000000] touch-none"
         onMouseDown={mouseDownHandler}
         onMouseMove={mouseMoveHandler}
         onMouseUp={mouseUpHandler}
